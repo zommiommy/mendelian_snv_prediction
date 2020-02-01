44b138bc64e16630c15de62d5c5206c1839f1407
zom <t.fontana@net2action.com>
2020-02-01 12:58:26 +0100

    updated run test for the new pipeline

--- tests/test_run.py
+++ tests/test_run.py
@@ -6,22 +6,23 @@ from holdouts_generator import holdouts_generator, balanced_random_holdouts
 def test_run():
     pwd = os.path.dirname(os.path.abspath(__file__))
 
-    mixed = get_data(
+    train, test = get_data(
         pwd + "/../mendelian_snv.csv.gz",
         assembly="hg19",
         batchsize=128,
         head_threshold=1e5,
         seed=1337
     )
+    
+    model = get_model()
+    model.summary()
 
-
-    classifier = get_model()
-    classifier.summary()
-
-    histoty = classifier.fit_generator(
-        generator=mixed,
-        steps_per_epoch=mixed.steps_per_epoch // 5,
-        epochs=2,
+    history = model.fit_generator(
+        generator=train,
+        steps_per_epoch=train.steps_per_epoch,
+        validation_data=test,
+        validation_steps=test.steps_per_epoch,
+        epochs=1,
         verbose=1,
         use_multiprocessing=False,
         shuffle=True
