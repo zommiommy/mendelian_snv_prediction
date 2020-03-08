b8ef59ac1af66472273b893e0df315947bcc9074
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-08 21:27:54 +0100

    Fixed errors in docker launch

--- run_docker.py
+++
@@ -1,2 +0,0 @@
-docker build -p 8888:8888 --file Dockerfile -t $(basename $PWD) .
-nvidia-docker run --tty --interactive  $(basename $PWD)
---
+++ run_docker.sh
@@ -0,0 +1,2 @@
+docker build  --file Dockerfile -t $(basename $PWD) .
+nvidia-docker run --tty --interactive --publish 8888:8888 $(basename $PWD)
