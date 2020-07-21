76b6312390aedd63f2504570a1690aab2d91ae14
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-07-21 11:20:48 +0200

    Reduced size of tensors shapes for testing

--- tests/test_training_shapes.py
+++ tests/test_training_shapes.py
@@ -3,21 +3,21 @@ from mendelian_snv_prediction import get_holdouts
 
 def test_training_shape():
     batch_size = 16
-    for window_size in (50, 501):
-        train, test = next(get_holdouts(
-            batch_size=batch_size,
-            window_size=window_size,
-            max_wiggle_size=10,
-            nrows=1000,
-            verbose=False
-        ))
-        assert train[0][0].shape == (
-            batch_size,
-            window_size,
-            4
-        )
-        assert test[0][0].shape == (
-            batch_size,
-            window_size,
-            4
-        )
+    window_size = 50
+    train, test = next(get_holdouts(
+        batch_size=batch_size,
+        window_size=window_size,
+        max_wiggle_size=10,
+        nrows=1000,
+        verbose=False
+    ))
+    assert train[0][0].shape == (
+        batch_size,
+        window_size,
+        4
+    )
+    assert test[0][0].shape == (
+        batch_size,
+        window_size,
+        4
+    )
